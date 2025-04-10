f950cf532cd83549c0a57d076637d901c1eda05e
Harshil-2005 <harshilgujarati2005@gmail.com>
2025-04-10 22:12:59 +0530

    Final commit with full Alterpy library and demo

--- alterpy/.ipynb_checkpoints/data_loader-checkpoint.py
+++
@@ -1,4 +0,0 @@
-# dummy DataLoader class
-class DataLoader:
-    def __init__(self, path): pass
-    def load_csv(self): pass
--- alterpy/analyzer.py
+++
@@ -1,4 +0,0 @@
-# dummy Analyzer class
-class Analyzer:
-    def __init__(self, df): pass
-    def attendance_percentage(self, col1, col2): pass
